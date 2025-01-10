b885b29680d0cab71976bf4d6d37696a77b25b4a
Lakshmi Prahitha Pendam <pendam@qti.qualcomm.com>
2025-01-10 11:04:10 +0530

    agm: Extend AGM interface for CSHM
    Implementation of CSHM APIs Alloc, dealloc and sendMsg.
    Add support for CSHM client handling and management.
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

--- ipc/aidl/aidlconverter/inc/agm/AgmLegacyToAidl.h
+++ ipc/aidl/aidlconverter/inc/agm/AgmLegacyToAidl.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -273,4 +274,6 @@ struct LegacyToAidl {
     */
     static void convertMmapBufferInfoToAidl(struct agm_buf_info *legacyBufferInfo,
                                             MmapBufInfo *aidlBufferInfo, int flags);
+
+    static aidl::android::hardware::common::NativeHandle convertfdToAidl(int fd);
 };
